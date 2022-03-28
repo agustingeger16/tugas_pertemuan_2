5d849978b27ca8ea1736432b68de92df1216de71
agustingeger16 <agustingeger@gmail.com>
2022-03-28 23:23:53 +0700

    menambahkan directory codeignert ke github, geger

---
+++ application/views/view-lathian.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>lathian 1</title>
+</head>
+<body>
+halo kawan.. yuk kita belajar web programming ..!!!<br>
+nilai 1 = <?= $nilai1; ?>
+nilai 2 = <?= $nilai2; ?>
+ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " .$hasil;?>
+
+</body>
+</html>
